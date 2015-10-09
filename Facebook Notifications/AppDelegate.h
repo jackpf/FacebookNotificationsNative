4a8d73c431eaf13e4908aaa37b9c281f6f069832
jackpf <jack_farrelly@yahoo.co.uk>
2015-10-09 09:13:38 +0100

    Initial Commit

---
+++ Facebook Notifications/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Facebook Notifications
+//
+//  Created by Jack Farrelly on 09/10/2015.
+//  Copyright (c) 2015 Jack Farrelly. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Facebook Notifications/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  Facebook Notifications
+//
+//  Created by Jack Farrelly on 09/10/2015.
+//  Copyright (c) 2015 Jack Farrelly. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
